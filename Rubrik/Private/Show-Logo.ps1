f197372d569de244401e2047c90ba0d8acc7fb86
Jaap Brasser <noreply@jaapbrasser.com>
2019-10-04 23:00:01 +0200

    Created Show-Logo private function

---
+++ Rubrik/Private/Show-Logo.ps1
@@ -0,0 +1,15 @@
+function Show-Logo {
+<#
+.SYNOPSIS
+Helper function that displays the Rubrik Logo and connection information
+#>
+    begin {
+        
+    }
+    
+    process {
+@"
+Display Logo
+"@
+    }
+}
